6ef98062c76f5b3a94e726c08baadf4f748b83a0
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2018-03-27 01:00:55 +0900

    [change]userモデルからemail項目削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,10 @@ class User < ApplicationRecord
     provider = auth[:provider]
     uid = auth[:uid]
     name = auth[:info][:name]
-    email = auth[:info][:email]
     image_url = auth[:info][:image]
 
     self.find_or_create_by(provider: provider, uid: uid) do |user|
       user.name = name
-      user.email = email
       user.image_url = image_url
     end
   end
