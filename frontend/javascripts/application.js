83d028c1866a1f9593ef4e4ea18530ef29044512
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2018-03-30 15:01:23 +0900

    [add]マイページ画面効果のjs作成

--- frontend/javascripts/application.js
+++ frontend/javascripts/application.js
@@ -1,2 +1,4 @@
 import 'bootstrap/dist/js/bootstrap';
 import './remove_unneseccary_hash';
+import './mypage_filename';
+import './mypage_submit';
---
+++ frontend/javascripts/mypage_submit.js
@@ -0,0 +1,9 @@
+//名前変更とファイル選択時に、更新ボタンを使用可能にする
+$(window).on('load', function(){
+  $('#mypage_name').on('input', enable_submit);
+  $('#mypage_filefield').on('change', enable_submit);
+
+  function enable_submit(){
+    $('#mypage_submit').removeClass('disabled opacity-02');
+  };
+});
