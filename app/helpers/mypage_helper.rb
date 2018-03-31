b2d5b463ffe6b3fb10c22252da09f4304d42590b
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2018-04-01 03:58:42 +0900

    [add]マイページ名前エラーメッセージ日本語化

--- app/helpers/mypage_helper.rb
+++ app/helpers/mypage_helper.rb
@@ -6,4 +6,10 @@ module MypageHelper
       'form-control form-control-lg text-purple mt-4 mt-md-0'
     end
   end
+
+  def mypage_name_error(msg)
+    content_tag :div, class: 'invalid-feedback d-block' do
+      I18n.t('activerecord.attributes.user.name') + msg
+    end
+  end
 end
