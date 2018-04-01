a7db5582fe4b0f6022eea1147fe6a2e4a302d4eb
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2018-04-01 09:33:40 +0900

    [add]Cloudinary設定

--- app/uploaders/my_image_uploader.rb
+++ app/uploaders/my_image_uploader.rb
@@ -3,8 +3,14 @@ class MyImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
+  if Rails.env.production? || Rails.env.staging?
+    include Cloudinary::CarrierWave
+  else
+    storage :file
+  end
+
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
