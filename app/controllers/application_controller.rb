28a189ad5209d9e3a783409dc18d2eaa471cbd10
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2018-05-04 13:07:59 +0900

    [fix]ログイン判定メソッドを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,12 @@ class ApplicationController < ActionController::Base
                                  password: ENV.fetch('BASIC_AUTH_PASSWORD')
   end
 
-  helper_method :logged_in?, :current_user, :logged_in_by_correct_user?
+  helper_method :logged_in?, :current_user, :logged_in?
 
   private
 
   def logged_in?
-    !!session[:user_id]
+    session[:user_id].present? && User.exists?(session[:user_id])
   end
 
   def current_user
@@ -19,12 +19,8 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id])
   end
 
-  def logged_in_by_correct_user?
-    logged_in? && User.exists?(session[:user_id])
-  end
-
   def authenticate
-    return if logged_in_by_correct_user?
+    return if logged_in?
     redirect_to login_path, alert: 'ログインしてください'
   end
 end
