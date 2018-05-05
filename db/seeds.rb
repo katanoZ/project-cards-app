734977150c7fc888a18a8ebde433c3670d19a410
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2018-05-05 13:18:13 +0900

    [add]マイプロジェクト用のシードデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,13 @@ end
     user_id: User.ids.sample,
   )
 end
+
+#my_projects（最初にアプリでユーザ登録してから、次にseedデータを作成する）
+my_user = User.first
+100.times do |n|
+  Project.create!(
+    name: "プロジェクト#{n + 1001}",
+    summary: Faker::Hacker.say_something_smart,
+    user: my_user,
+  )
+end
