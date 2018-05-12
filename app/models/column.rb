0a65b536301f71c6ce920eb0f06464dd457a7f51
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2018-05-12 10:31:53 +0900

    [add]プロジェクト詳細のカラム移動機能を追加

--- app/models/column.rb
+++ app/models/column.rb
@@ -3,4 +3,8 @@ class Column < ApplicationRecord
   acts_as_list scope: :project
 
   validates :name, presence: true, uniqueness: { scope: :project }, length: { maximum: 40 }
+
+  before_destroy do
+    remove_from_list
+  end
 end
