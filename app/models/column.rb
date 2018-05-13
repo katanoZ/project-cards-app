cb60482cfc7c2789d3e61f7a0b6a3f726170d247
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2018-05-13 14:26:21 +0900

    [fix]プロジェクト詳細のカラム表示順の記述を修正

--- app/models/column.rb
+++ app/models/column.rb
@@ -3,8 +3,4 @@ class Column < ApplicationRecord
   acts_as_list scope: :project
 
   validates :name, presence: true, uniqueness: { scope: :project }, length: { maximum: 40 }
-
-  before_destroy do
-    remove_from_list
-  end
 end
