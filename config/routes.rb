2242d0d7730be5009a120eeffa0c5dcc63dec8ef
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2018-05-13 12:26:20 +0900

    [clean]omniauthログイン用のルートを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root 'projects#index'
 
   get 'login', to: 'sessions#new'
-  get 'auth/:provider/callback', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/:id', to: 'sessions#auth', as: :auth
 
   get 'mypage', to: 'users#edit'
   patch 'mypage', to: 'users#update'
