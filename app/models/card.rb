a04c2b4688e31ff0e28d50ec3543ac702898d454
Katanosaka Hiroaki <katano9@yahoo.co.jp>
2018-05-21 19:19:23 +0900

    [add]ログ生成機能を作成

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,10 +5,17 @@ class Card < ApplicationRecord
   belongs_to :project
   belongs_to :column
 
+  attr_accessor :operator
+
   acts_as_list scope: %i[project_id column_id]
 
   validates :name, presence: true,
                    uniqueness: { scope: :project },
                    length: { maximum: 40 }
   validates :due_date, allow_nil: true, due_date_range: true
+
+  after_create CardLogsCallbacks.new
+  after_save CardLogsCallbacks.new
+  after_update CardLogsCallbacks.new
+  after_destroy CardLogsCallbacks.new
 end
